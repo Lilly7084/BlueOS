7b55e59948db35b57ce6ec2b49b4096830f28576
Lilly7084 <wmays287@gmail.com>
2024-05-11 19:15:43 -0700

    The rest of the filesystem driver

--- 2a3351d6-77f3-9cf0-2be5-0732aaeded03/System/Libraries/Filesystem.lua
+++ 2a3351d6-77f3-9cf0-2be5-0732aaeded03/System/Libraries/Filesystem.lua
@@ -54,7 +54,7 @@ local pathAndName = function (path)
     local segments, absolute = Filesystem.segments(path)
     local nameComp = segments[#segments]
     table.remove(segments, #segments)
-    local pathComp = table.concat(segments, "/")
+    local pathComp = table.concat(segments, "/") .. "/"
     return pathComp, nameComp
 end
 
@@ -191,37 +191,176 @@ end
 
 -- Returns an iterator over all the items in the directory at a path
 Filesystem.list = function (path)
-    error("Filesystem.list() not yet implemented!")
+    local proxy, proxyPath = Filesystem.get(path)
+    if not proxy then return nil, proxyPath end
+    local list, reason = proxy.list(proxyPath)
+    if not list then return nil, reason end
+    -- Add in any mounted paths which exist in the same folder
+    for _, mount in ipairs(mounts) do
+        local pathComp, nameComp = pathAndName(mount.path)
+        if pathComp == path and nameComp then
+            table.insert(list, nameComp .. "/")
+        end
+    end
+    return list
 end
 
 -- Creates a directory at the specified path
 Filesystem.makeDirectory = function (path)
-    error("Filesystem.makeDirectory() not yet implemented!")
+    local proxy, proxyPath = Filesystem.get(path)
+    if not proxy then return nil, proxyPath end
+    return proxy.makeDirectory(proxyPath)
 end
 
 -- Removes the item at the specified path. Recursively empties out folders.
 Filesystem.remove = function (path)
-    error("Filesystem.remove() not yet implemented!")
+    local proxy, proxyPath = Filesystem.get(path)
+    if not proxy then return nil, proxyPath end
+    -- Clear out all the items inside if this is a folder
+    if proxy.isDirectory(proxyPath) then
+        local list, reason = proxy.list(proxyPath)
+        if not list then return nil, reason end
+        for _, item in ipairs(list) do
+            Filesystem.remove(Filesystem.concat(path, item))
+        end
+    end
+    -- Do the actual deletion now
+    return proxy.remove(proxyPath)
 end
 
 -- Renames a file or folder. Can copy files if the paths are on different
 -- volumes, but cannot copy folders.
 Filesystem.rename = function (oldPath, newPath)
-    error("Filesystem.rename() not yet implemented!")
+    local oldProxy, oldProxyPath = Filesystem.get(oldPath)
+    if not oldProxy then return nil, oldProxyPath end
+    local newProxy, newProxyPath = Filesystem.get(newPath)
+    if not newProxy then return nil, newProxyPath end
+    -- Bail out now in case of anything that would stop us later
+    if oldProxy.isDirectory(oldProxyPath) then
+        return nil, "Is a directory"
+    end
+    if newProxy.exists(newProxyPath) then
+        return nil, "Item already exists"
+    end
+    -- If the volumes are different, copy then delete the original
+    if oldProxy.address ~= newProxy.address then
+        Filesystem.copy(oldPath, newPath) -- This runs .get() again but meh
+        return oldProxy.remove(oldProxyPath)
+    end
+    -- Otherwise it really is just renaming the file
+    return oldProxy.rename(oldProxyPath, newProxyPath)
 end
 
 -- Copies a file from one location to another.
 Filesystem.copy = function (fromPath, toPath)
-    error("Filesystem.copy() not yet implemented!")
+    local from = Filesystem.open(fromPath, "r")
+    local to = Filesystem.open(toPath, "w")
+    local chunk, reason
+    repeat
+        chunk, reason = from:read()
+        to:write(chunk or "")
+    until not chunk or string.len(chunk) == 0
+    from:close()
+    to:close()
+    if reason then
+        return nil, reason
+    end
+    return true
 end
 
 --------------------------------------------------------------------------------
--- File handles (TODO)
+-- File handles
+
+local fileRead = function (self, n)
+    checkArg(1, n, "number", "nil")
+    n = n or math.huge
+    local buffer = ""
+    repeat
+        local chunk, reason = self.proxy.read(self.stream, n)
+        if not chunk and reason then
+            return nil, reason
+        end
+        buffer = buffer .. (chunk or "")
+    until not chunk
+    self.offset = self.offset + string.len(buffer)
+    return buffer
+end
+
+local fileWrite = function (self, str)
+    checkArg(1, str, "string")
+    local result, reason = self.proxy.write(self.stream, str)
+    if result then
+        self.offset = self.offset + string.len(str)
+    end
+    return result, reason
+end
+
+local fileSeek = function (self, whence, offset)
+    checkArg(1, whence, "string")
+    checkArg(1, offset, "number", "nil")
+    offset = offset or 0
+    if whence == "set" then
+        -- Relative to start of file
+        local result, reason = self.proxy.seek(self.stream, "set", offset)
+        if result then
+            self.offset = result
+        end
+        return result, reason
+    elseif whence == "cur" then
+        -- Relative to current position
+        local result, reason = self.proxy.seek(self.stream, "set", self.offset + offset)
+        if result then
+            self.offset = result
+        end
+        return result, reason
+    elseif whence == "end" then
+        -- Relative to end of file
+        local result, reason = self.proxy.seek(self.stream, "end", offset)
+        if result then
+            self.offset = result
+        end
+        return result, reason
+    else
+        -- Invalid
+        return nil, "Invalid seek mode"
+    end
+end
+
+local fileClose = function (self)
+    return self.proxy.close(self.stream)
+end
 
 Filesystem.open = function (path, mode)
-    error("Filesystem.open() not yet implemented!")
+    checkArg(1, path, "string")
+    checkArg(1, mode, "string", "nil")
+    mode = mode or "r"
+    local proxy, proxyPath = Filesystem.get(path)
+    if not proxy then return nil, proxyPath end
+    local handle, reason = proxy.open(proxyPath, mode)
+    if not handle then return nil, reason end
+    -- Construct the file proxy
+    local file = {
+        proxy = proxy,
+        stream = handle,
+        offset = 0,
+        seek = fileSeek,
+        close = fileClose
+    }
+    -- Add in the extra methods which are specific to certain modes
+    if mode == "r" or mode == "rb" then
+        -- Read
+        file.read = fileRead
+    elseif mode == "w" or mode == "wb" or mode == "a" or mode == "ab" then
+        -- Write
+        file.write = fileWrite
+    else
+        -- Invalid
+        file:close() -- Clean up
+        return nil, "Invalid access mode"
+    end
+    return file
 end
 
 --------------------------------------------------------------------------------
-Filesystem.mounts = {}
+Filesystem.mounts = mounts
 return Filesystem
